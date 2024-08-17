7df5d5d8d1fedd32946aa6b82164c993098ad7f0
Nicoli Martins Freitas <nicomart@c2r1p2.42sp.org.br>
2024-08-17 12:45:57 -0300

    Rush04

--- rush04.c
+++ rush04.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   e.c                                                :+:      :+:    :+:   */
+/*   rush04.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhermini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 10:27:11 by mhermini          #+#    #+#             */
-/*   Updated: 2024/08/17 10:27:22 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/08/17 12:29:42 by nicomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,24 @@ void	rush(int x, int y)
 	int	put_y;
 
 	put_x = 1;
-	put_y = 1;
 	while (put_y <= y)
 	{
+		put_x = 1;
 		while (put_x <= x)
 		{
 			if (put_x == 1 && put_y == 1
 				|| put_x == x && put_y == y && put_x != 1 && put_y != 1)
 				write (1, "A", 1);
 			else if (put_x == x && put_y == 1
-					|| put_x == 1 && put_y == y )
-					write (1, "C", 1);
-	 			else if (put_x == 1 || put_x == x || put_y == 1 || put_y == y)
-	 				  	write (1, "B", 1);
-	 			   else
-	 				  write (1, " ", 1);
-	 		put_x +=1;
+				|| put_x == 1 && put_y == y)
+				write (1, "C", 1);
+			else if (put_x == 1 || put_x == x || put_y == 1 || put_y == y)
+				write (1, "B", 1);
+			else
+				write (1, " ", 1);
+		put_x += 1;
 		}
-		put_x = 1;
 		write (1, "\n", 1);
-		put_y +=1;
+		put_y += 1;
 	}
 }
